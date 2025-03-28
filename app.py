f2bc40ab2eb73308b1aa07aaa2e31928fa36d635
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-03-28 15:56:10 +0500

    learning

---
+++ app.py
@@ -0,0 +1,7 @@
+# 1.  opens a file, reads its contents, and prints them
+with open("story.txt" , "r") as file:  # we open a file story.txt and name it as file
+  story = file.read() # 2. we store the file read into the variable story
+  print(story) # 3. we print the file content finally
+  
+# 2. writes data to a file
+# Create a program that writes your name and age to a file called my_info.txt

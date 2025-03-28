4021d293ba51741fea42bd726c24361b2e8b0f73
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-03-28 17:14:04 +0500

    learning

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ with open("story.txt" , "r") as file:  # we open a file story.txt and name it as
   print(story) # 3. we print the file content finally
   
 # 2. writes data to a file
-# Create a program that writes your name and age to a file called my_info.txt
+# Create a program that writes your name and profession to a file called my_info.txt
+
+with open("my_info.txt" , "w") as file:
+  file.write("my name is sadaf and i m front end developer.")
+  
